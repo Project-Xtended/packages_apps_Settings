bb491fb6061ebb909b8044f7c93ce23a0378d8bd
Harry Youd <harry@harryyoud.co.uk>
2017-09-27 19:48:42 +0100

    Settings: Add Updater
    Adding to both "System" (top category), and "About phone"
    (subcategory of "System"), similar to that of the GMS System Update.
    
    AOSiP Edits:
     - Removed from "About Phone"
     - Fixed for 10

--- src/com/android/settings/deviceinfo/aboutphone/MyDeviceInfoFragment.java
+++ src/com/android/settings/deviceinfo/aboutphone/MyDeviceInfoFragment.java
@@ -40,6 +40,7 @@ import com.android.settings.deviceinfo.UpTimePreferenceController;
 import com.android.settings.deviceinfo.WifiMacAddressPreferenceController;
 import com.android.settings.deviceinfo.imei.ImeiInfoPreferenceController;
 import com.android.settings.deviceinfo.simstatus.SimStatusPreferenceController;
+import com.android.settings.deviceinfo.UpdatePreferenceController;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settings.widget.EntityHeaderController;
 import com.android.settingslib.core.AbstractPreferenceController;
@@ -108,6 +109,7 @@ public class MyDeviceInfoFragment extends DashboardFragment
         controllers.add(new SafetyInfoPreferenceController(context));
         controllers.add(new UpTimePreferenceController(context));
 	controllers.add(new SELinuxStatusPreferenceController(context));
+	controllers.add(new UpdatePreferenceController(context));
         return controllers;
     }
 
