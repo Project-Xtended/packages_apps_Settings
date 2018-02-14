nitrogen-project/android_packages_apps_Settings

Export ButtonSettings for tile

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -132,6 +132,7 @@ import com.android.settings.wifi.WifiInfo;
 import com.android.settings.wifi.WifiSettings;
 import com.android.settings.wifi.p2p.WifiP2pSettings;
 
+import com.nitrogen.settings.fragments.ButtonSettings;
 import com.nitrogen.settings.fragments.HeadsUpSettings;
 import com.nitrogen.settings.NitrogenSettings;
 import com.nitrogen.settings.fragments.FlingSettings;
@@ -265,7 +266,8 @@ public class SettingsGateway {
             NavbarSettings.class.getName(),
             FlingSettings.class.getName(),
             SmartbarSettings.class.getName(),
-            PulseSettings.class.getName()
+            PulseSettings.class.getName(),
+            ButtonSettings.class.getName()
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {

Project-Xtended/packages_apps_Settings

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -136,6 +136,8 @@ import com.xtended.xtensions.fragments.FlingSettings;
 import com.xtended.xtensions.fragments.NavbarSettings;
 import com.xtended.xtensions.fragments.PulseSettings;
 import com.xtended.xtensions.fragments.SmartbarSettings;
+import com.xtended.xtensions.fragments.ButtonSettings;
+import com.xtended.xtensions.fragments.HeadsUpSettings;
 
 public class SettingsGateway {
 
@@ -263,7 +265,8 @@ public class SettingsGateway {
             NavbarSettings.class.getName(),
             FlingSettings.class.getName(),
             SmartbarSettings.class.getName(),
-            PulseSettings.class.getName()
+            PulseSettings.class.getName(),
+            ButtonSettings.class.getName()
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {