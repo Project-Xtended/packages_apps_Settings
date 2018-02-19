733be3a6c114a1c0d800898bda7ee9050d89a759
xyyx <xyyx@mail.ru>
2018-02-19 10:30:16 +0800

    Changelog: Add left padding
    before: https://imgur.com/a/TzTAD
    after: https://imgur.com/a/EPjqT
    Signed-off-by: DennySPB <dennyspb@gmail.com>

--- src/com/android/settings/deviceinfo/Changelog.java
+++ src/com/android/settings/deviceinfo/Changelog.java
@@ -20,6 +20,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.LinearLayout.LayoutParams;
 import android.widget.ScrollView;
 import android.widget.TextView;
 
@@ -63,6 +65,9 @@ public class Changelog extends SettingsPreferenceFragment {
         }
 
         final TextView textView = new TextView(getActivity());
+        LinearLayout.LayoutParams llp = new LinearLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+        llp.setMargins(20, 0, 0, 0); // llp.setMargins(left, top, right, bottom);
+        textView.setLayoutParams(llp);
         textView.setText(text);
 
         final ScrollView scrollView = new ScrollView(getActivity());
