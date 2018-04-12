6c6614cc85972672bc1169649661b89a9bd0cde0
Adarsh-MR <adarshmr1998@gmail.com>
2018-04-12 13:48:50 +0000

    Themes: Settings: fix storage icon tint
    * Commit: SubstratumResources/platform_packages_apps_settings@8af7a45
      changes storage summary and icon color to textColorSecondary due to that
      themers could not theme it accent
    
    * since colorControlNormal is the attrs used everywhere on settings for icons,
      change textColorSecondary to colorControlNormal
    
    * themes already changes colorControlNormal to colorAccent in base style
    
    Change-Id: I456b34fbaaae2b78e4c9053b3d1da80de31cbeaf
    Signed-off-by: Adarsh-MR <adarshmr1998@gmail.com>

--- src/com/android/settings/deviceinfo/StorageVolumePreference.java
+++ src/com/android/settings/deviceinfo/StorageVolumePreference.java
@@ -46,7 +46,7 @@ public class StorageVolumePreference extends Preference {
     private final VolumeInfo mVolume;
 
     private int mColor;
-    private int mSecondaryColor;
+    private int mIconColor;
     private int mUsedPercent = -1;
 
     // TODO: ideally, VolumeInfo should have a total physical size.
@@ -57,7 +57,7 @@ public class StorageVolumePreference extends Preference {
         mVolume = volume;
         mColor = color;
 
-        mSecondaryColor = Utils.getColorAttr(context, android.R.attr.textColorSecondary);
+        mIconColor = Utils.getColorAttr(context, android.R.attr.colorControlNormal);
 
         setLayoutResource(R.layout.storage_volume);
 
@@ -99,7 +99,7 @@ public class StorageVolumePreference extends Preference {
         }
 
         icon.mutate();
-        icon.setTint(mColor);
+        icon.setTint(mIconColor);
         setIcon(icon);
 
         if (volume.getType() == VolumeInfo.TYPE_PUBLIC
@@ -113,7 +113,7 @@ public class StorageVolumePreference extends Preference {
     public void onBindViewHolder(PreferenceViewHolder view) {
         final ImageView unmount = (ImageView) view.findViewById(R.id.unmount);
         if (unmount != null) {
-            unmount.setImageTintList(ColorStateList.valueOf(mSecondaryColor));
+            unmount.setImageTintList(ColorStateList.valueOf(mIconColor));
             unmount.setOnClickListener(mUnmountListener);
         }
 
