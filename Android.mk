3f78131e84e7b15c88c037a75ebe078f510ad3d5
a.derendyaev <a.derendyaev@magdv.com>
2018-11-14 10:54:09 +0700

    AmbientPlay: fix crash in history
    Change-Id: I35a4f53215b852f441aa9ea782613ba3c6ed8f13
    Signed-off-by: a.derendyaev <a.derendyaev@magdv.com>

--- Android.mk
+++ Android.mk
@@ -56,7 +56,6 @@ LOCAL_STATIC_JAVA_LIBRARIES := \
 LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res \
     frameworks/support/v7/preference/res \
     frameworks/support/v14/preference/res \
-    frameworks/support/v7/appcompat/res \
     frameworks/support/v7/recyclerview/res \
     packages/apps/Xtensions/res \
     packages/apps/XOta/res
@@ -67,7 +66,6 @@ LOCAL_AAPT_FLAGS := --auto-add-overlay \
     --extra-packages android.support.v7.preference \
     --extra-packages android.support.v14.preference \
     --extra-packages android.support.v17.preference \
-    --extra-packages android.support.v7.appcompat \
     --extra-packages android.support.v7.recyclerview \
     --extra-packages com.msm.xtended \
     --extra-packages com.xtended.settings
