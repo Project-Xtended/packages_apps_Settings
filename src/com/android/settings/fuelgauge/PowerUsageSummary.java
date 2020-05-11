e87a77483d7a323077224d23bb70d0cfb9206d45
SuperDroidBond <superdroidbond@yahoo.com>
2020-05-11 19:59:48 +0530

    BatteryHealth: make resources overlayable
    Not every device have the charge full & charge full design files at the same location or same name.
    Better to keep it overlayable for maintainer to pick.
    Overlay would be for Settings - overlay/packages/apps/Settings/res/values/x_config.xml
    
    For Example -:
    
        <!-- Battery Health -->
        <string name="config_batDesCap">/sys/class/power_supply/bms/charge_full</string>
        <string name="config_batCurCap">/sys/class/power_supply/bms/charge_now_raw</string>
    
    Change-Id: I1ea1fae71064fe072d03013542927f2df2e0c319
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>
    Signed-off-by: spezi77 <spezi7713@gmx.net>
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- src/com/android/settings/fuelgauge/PowerUsageSummary.java
+++ src/com/android/settings/fuelgauge/PowerUsageSummary.java
@@ -82,10 +82,8 @@ public class PowerUsageSummary extends PowerUsageBase implements OnLongClickList
     private static final String KEY_CURRENT_BATTERY_CAPACITY = "current_battery_capacity";
     private static final String KEY_DESIGNED_BATTERY_CAPACITY = "designed_battery_capacity";
 
-    private static final String FILENAME_BATTERY_DESIGN_CAPACITY =
-            "/sys/class/power_supply/bms/charge_full_design";
-    private static final String FILENAME_BATTERY_CURRENT_CAPACITY =
-            "/sys/class/power_supply/bms/charge_full";
+    private String mBatDesCap;
+    private String mBatCurCap;
 
     @VisibleForTesting
     static final int BATTERY_INFO_LOADER = 1;
@@ -341,13 +339,17 @@ public class PowerUsageSummary extends PowerUsageBase implements OnLongClickList
         } else {
             mNeedUpdateBatteryTip = true;
         }
+
+        mBatDesCap = getResources().getString(R.string.config_batDesCap);
+        mBatCurCap = getResources().getString(R.string.config_batCurCap);
+
         // reload BatteryInfo and updateUI
         restartBatteryInfoLoader();
         updateLastFullChargePreference();
         mScreenUsagePref.setSummary(StringUtil.formatElapsedTime(getContext(),
                 mBatteryUtils.calculateScreenUsageTime(mStatsHelper), false));
-        mCurrentBatteryCapacity.setSubtitle(parseBatterymAhText(FILENAME_BATTERY_CURRENT_CAPACITY));
-        mDesignedBatteryCapacity.setSubtitle(parseBatterymAhText(FILENAME_BATTERY_DESIGN_CAPACITY));
+        mCurrentBatteryCapacity.setSubtitle(parseBatterymAhText(mBatCurCap));
+        mDesignedBatteryCapacity.setSubtitle(parseBatterymAhText(mBatDesCap));
         mBatteryTemp.setSubtitle(
                 XtendedUtils.mccCheck(getContext()) ?
                 XtendedUtils.batteryTemperature(getContext(), true) + "°F" :
