2852534e13b2d12f7d4dc4da4d5c6539049a8efe
kry9ton <dhimasbagusprayoga@gmail.com>
2020-07-22 22:50:19 +0530

    GVM chooser adjustment [2/2]
    Change-Id: I24e9af54ad9d64d9d8479f1c5abe3fa4f52eaf36

--- src/com/android/settings/xtended/XThemeRoom.java
+++ src/com/android/settings/xtended/XThemeRoom.java
@@ -137,6 +137,10 @@ public class XThemeRoom extends DashboardFragment implements
                 "android.theme.customization.adaptive_icon_shape"));
         controllers.add(new OverlayCategoryPreferenceController(context,
                 "android.theme.customization.icon_pack.android"));
+	controllers.add(new OverlayCategoryPreferenceController(context,
+		"android.theme.customization.statusbar_height"));
+	controllers.add(new OverlayCategoryPreferenceController(context,
+		"android.theme.customization.ui_radius"));
         return controllers;
     }
 
