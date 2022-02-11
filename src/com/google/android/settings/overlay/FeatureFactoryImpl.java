907b6ebafc3087efbb15260dc8bb32e99d1b5737
DennySPb <dennyspb@gmail.com>
2022-02-11 16:13:24 +0300

    Option to change PowerUsage provider
        * Google or AOSP one
    
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Change-Id: I3e3e9065ca5be98734799ccf7a4791a5d35d432a
    Signed-off-by: blinoff82 <blinov.in@gmail.com>
    Signed-off-by: mukesh22584 <mks22584@gmail.com>

--- src/com/google/android/settings/overlay/FeatureFactoryImpl.java
+++ src/com/google/android/settings/overlay/FeatureFactoryImpl.java
@@ -2,21 +2,31 @@ package com.google.android.settings.overlay;
 
 import android.content.Context;
 
+import android.os.SystemProperties;
+
 import com.android.settings.fuelgauge.PowerUsageFeatureProvider;
 import com.android.settings.accounts.AccountFeatureProvider;
 import com.google.android.settings.accounts.AccountFeatureProviderGoogleImpl;
+import com.android.settings.fuelgauge.PowerUsageFeatureProviderImpl;
 import com.google.android.settings.fuelgauge.PowerUsageFeatureProviderGoogleImpl;
 
 public final class FeatureFactoryImpl extends com.android.settings.overlay.FeatureFactoryImpl {
 
     private PowerUsageFeatureProvider mPowerUsageFeatureProvider;
     private AccountFeatureProvider mAccountFeatureProvider;
+    private final boolean isPowerUsageProviderGoogle =
+                                  SystemProperties.getBoolean("persist.powerusage_provider_google" , true);
 
     @Override
     public PowerUsageFeatureProvider getPowerUsageFeatureProvider(Context context) {
         if (mPowerUsageFeatureProvider == null) {
-            mPowerUsageFeatureProvider = new PowerUsageFeatureProviderGoogleImpl(
-                    context.getApplicationContext());
+            if (isPowerUsageProviderGoogle) {
+                mPowerUsageFeatureProvider = new PowerUsageFeatureProviderGoogleImpl(
+                        context.getApplicationContext());
+            } else {
+                 mPowerUsageFeatureProvider = new PowerUsageFeatureProviderImpl(
+                        context.getApplicationContext());
+            }
         }
         return mPowerUsageFeatureProvider;
     }
