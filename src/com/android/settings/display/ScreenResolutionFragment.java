StagOS/android_packages_apps_Settings

Change dpi on changing screen resolution * Needs https://github.com/StagOS/android_frameworks_base/commit/cedbd830d61dfcf237ef4482c6020e93cb5de652 * Also requires config_screen_diagonal_length in dtree

src/com/android/settings/display/ScreenResolutionFragment.java
@@ -40,6 +40,8 @@ import com.android.settingslib.widget.FooterPreference;
 import com.android.settingslib.widget.IllustrationPreference;
 import com.android.settingslib.widget.SelectorWithWidgetPreference;
 
+import com.android.internal.util.stag.StagUtils;
+
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
@@ -135,6 +137,11 @@ public class ScreenResolutionFragment extends RadioButtonPickerFragment {
     private Display.Mode getPreferMode(int width) {
         for (Point resolution : mResolutions) {
             if (resolution.x == width) {
+	        final String screenDiagonalLength = getResources().getString(
+                     R.string.config_screen_diagonal_length);
+		if (screenDiagonalLength != null) {
+		    StagUtils.changeScreenDPI(resolution.x, resolution.y, Float.parseFloat(screenDiagonalLength));
+		}
                 return new Display.Mode(
                         resolution.x, resolution.y, getDisplayMode().getRefreshRate());
             }

Project-Xtended/packages_apps_Settings

Signed-off-by: SuperDroidBond <superdroidbond@yahoo.com>

src/com/android/settings/display/ScreenResolutionFragment.java
@@ -45,6 +45,8 @@ import com.android.settingslib.widget.FooterPreference;
 import com.android.settingslib.widget.IllustrationPreference;
 import com.android.settingslib.widget.SelectorWithWidgetPreference;
 
+import com.android.internal.util.xtended.XtendedUtils;
+
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
@@ -145,6 +147,11 @@ public class ScreenResolutionFragment extends RadioButtonPickerFragment {
     private Display.Mode getPreferMode(int width) {
         for (Point resolution : mResolutions) {
             if (resolution.x == width) {
+	        final String screenDiagonalLength = getResources().getString(
+                     R.string.config_screen_diagonal_length);
+		if (screenDiagonalLength != null) {
+		    XtendedUtils.changeScreenDPI(resolution.x, resolution.y, Float.parseFloat(screenDiagonalLength));
+		}
                 return new Display.Mode(
                         resolution.x, resolution.y, getDisplayMode().getRefreshRate());
             }